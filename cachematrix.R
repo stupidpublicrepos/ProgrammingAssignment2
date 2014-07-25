1add2cd6261b777276251df711f5df821b533344
stupidpublicrepo <stupidpublicrepo@example.com>
2014-07-25 07:26:45 -0700

    Initial commit of makeCacheMatrix and cacheSolve functions.
    The former augments a matrix with set/get and setinverse/getinverse functions which set/get the underlying matrix object and its inverse cache respectively.
    
    The latter uses the augmented matrix object to provide a memo-ized solve function that has access to the augmented matrix's cached inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions establishes, and provides access to, an augmented 
+## matrix object that has a cache for its inverse.
 
-## Write a short comment describing this function
+## This function creates the augmented "matrix" object that can cache 
+## its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # i represents the cached inverse value
+  i <- NULL
+  
+  # set updates the object with a new matrix, clearing the cached inverse
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  # get returns the underlying matrix object
+  get <- function() x
+  
+  # setinverse caches the supplied inverse value
+  setinverse <- function(inverse) i <<- inverse
+  
+  # getinverse returns the cached inverse value
+  getinverse <- function() i
+  
+  # return a list with the functions that close over the 
+  # matrix x and its inverse i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function returns the inverse of the augmented "matrix" returned 
+## by makeCacheMatrix above. If the inverse has not already been calculated 
+## cacheSolve calculates it and caches it.  Finally cacheSolve returns the
+## cached value.
+## 
+## The invariant that the cached value (when present) always corresponds
+## to the matrix value is maintained by the set function of the augmented
+## matrix object which clears the cache when setting a new matrix value
+## thereby signalling the need to calculate the inverse anew.
+##
+## Example:
+##
+## > z <- rnorm(1000 * 1000)
+## > m <- matrix(z, 1000, 1000)
+## > cm <- makeCacheMatrix(m)
+##
+## > system.time(cacheSolve(cm))
+##   user  system elapsed 
+##   1.192   0.002   1.194 
+##
+## > system.time(cacheSolve(cm))
+## getting cached data
+##   user  system elapsed 
+##   0.001   0.000   0.001 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  # check the cache for an existing inverse 
+  i <- x$getinverse()
+  
+  if (is.null(i)) {
+    # cache miss, caculate 
+    data <- x$get()
+    i <- solve(data, ...)
+    
+    # store invese in cache
+    x$setinverse(i)
+  } else {
+    # cache hit, share the good news
+    message("getting cached data")
+  }
+  
+  # at this point the inverse was either in the cache initially
+  # or we just calculated it and put it there. Regardless, i is 
+  # the inverse now and we can return it.
+  i
 }
